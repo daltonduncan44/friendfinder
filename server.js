fd47a3a4537412908f2cc8a33435c024375d2151
Dalton Duncan <daltonduncan44@gmail.com>
2018-08-23 14:07:10 -0400

    survey connected to home

--- server.js
+++ server.js
@@ -28,3 +28,7 @@ app.use(express.static('app'));
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./app/public/home.html"));
   });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+  });
