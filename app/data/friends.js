11800557ff79b385d4ca562d15c1b0f8ba537002
Dalton Duncan <daltonduncan44@gmail.com>
2018-08-23 15:37:21 -0400

    friends.js made

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [ {
+	"name" : "Dalton",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/t31.0-8/13116244_10107568960519320_7024834722285826624_o.jpg",
+	"scores" : [5,5,2,2,3,5,1,2,1,2]
+}, {
+	"name" : "Jon",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13315379_10107649806588150_314898033587027263_n.jpg?oh=54f8e3f52844994423b3fec65e9327fb&oe=57E96499",
+	"scores" : [1,2,3,4,5,5,4,3,2,1]
+},{
+	"name" : "Matty",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13315379_10107649806588150_314898033587027263_n.jpg?oh=54f8e3f52844994423b3fec65e9327fb&oe=57E96499",
+	"scores" : [3,4,5,1,5,5,1,5,2,5]
+},{
+	"name" : "Cade",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13315379_10107649806588150_314898033587027263_n.jpg?oh=54f8e3f52844994423b3fec65e9327fb&oe=57E96499",
+	"scores" : [4,2,3,4,5,4,4,3,3,1]
+}, {
+	"name" : "Cj",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13450242_1233540543375869_9163241940047961665_n.jpg?oh=bb6cd85e9090d902fa663ff864f440e5&oe=5821AB84",
+	"scores" : [1,3,1,4,5,2,3,1,1,3]
+}];
+
+
+module.exports = friends;
